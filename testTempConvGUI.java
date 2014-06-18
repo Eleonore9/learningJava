69d1180a1e907bedb32c3b33be1f21bc02c3cd82
Eleonore9 <eleonore.mayola@ymail.com>
2014-06-18 22:10:38 +0200

    converter with GUI, trying to add a test

---
+++ testTempConvGUI.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static static org.junit.Assert.*;
+
+public class testTempConvGUI {
+
+    @Test
+    public void testmain() {
+        TempConvGUI tempConv = new testTempConvGUI();
+
+        
