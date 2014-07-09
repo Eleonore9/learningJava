ed649a1a336a00f74e1b16cb981746209197e3ac
Eleonore9 <eleonore.mayola@ymail.com>
2014-07-09 15:53:15 +0200

    added comments in the TempConv program

--- TempConv.java
+++ TempConv.java
@@ -1,15 +1,23 @@
-import java.util.Scanner;
+import java.util.Scanner; //To get user input from the command line
 
 public class TempConv {
+    /** 
+     * Converts a temperature in degrees
+     * fahrenheit to the equivalent in
+     * degrees celsius
+     */
     public static void main(String[] args) {
-        Double fahr;
-        Double cel;
-        Scanner in;
+        Double fahr; //Temperature in fahrenheit will have a type Double
+        Double cel; //same for the temperature in celsius
+        Scanner input; //Command line input is of type Scanner
         
-        in = new Scanner(System.in);
+        input = new Scanner(System.in);
         System.out.println("Enter the temperature in F: ");
-        if (in.hasNextDouble()) {
-            fahr = in.nextDouble();
+
+        /* if the user input is not a number I directly 
+         * return a message to tell the user to enter a number*/
+        if (input.hasNextDouble()) {
+            fahr = input.nextDouble();
             
             cel = (fahr - 32) * 5.0 / 9.0;
             System.out.println("Temperature in C is: " + cel);
