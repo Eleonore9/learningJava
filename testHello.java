1cad2de87a6e1d8ca139697059a810b8aeea8f9a
Eleonore9 <eleonore.mayola@ymail.com>
2014-07-09 18:27:07 +0200

    so therthere is an issue with testing standard output, I want to use system rules for that, i cannot import the system-rules package

--- testHello.java
+++ testHello.java
@@ -1,12 +1,20 @@
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.ClassRule.*;
+import org.junit.rules.*;
+import org.junit.Assert.*;
+import static system-rules.org.unit.contrib.java.lang.system.StandardOutputStreamLog.*;
 
 public class testHello {
+    @Rule
+    public final StandardOutputStreamLog outputLog = new StandardOutputStreamLog();
+
     @Test
     public void testPrintHelloWorld() {
-        Hello tester = new Hello();
-
-        String result = tester.main().toString();
-        assertEquals("Hello World!", result);
+        //Hello tester = new Hello();
+        //String result = tester.main().toString();
+        //assertEquals("Hello World!", result);
+        
+        System.out.print("Hello World!");
+        assertEquals("Hello World!", outputLog.getLog());
     }
 }
